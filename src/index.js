90504b354b0362c368fa5d40d9132e32227a21d3
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-03-06 20:36:46 -0500

    Cleanup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App/App";
 import "./index.css";
-//import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(<App />, document.getElementById("root"));
